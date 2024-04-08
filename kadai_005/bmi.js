55722e00a19daf0e5b2537b4da9019f3d0f575f7
tya-syu-wanntannmenn <157363231+tya-syu-wanntannmenn@users.noreply.github.com>
2024-04-08 23:46:06 +0900

    フォルダをアップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,9 @@
+let heightNumber;
+height = 1.7;
+let HeightNumber;
+heightNumber = height * height;
+let bodyWeight;
+bodyWeight = 68;
+let bmi;
+bmi = bodyWeight / heightNumber;
+console.log(bmi);
